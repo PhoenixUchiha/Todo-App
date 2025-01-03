0cdd2d8fef8abedd21937c84e9ca7925c4fcf0d0
PhoenixUchiha <108752038+PhoenixUchiha@users.noreply.github.com>
2025-01-03 23:17:21 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Todo from './assets/components/Todo'
+
+const App = () => {
+  return (
+    <div className='bg-gray-800 grid py-4 min-h-screen'>
+      <Todo></Todo>
+    </div>
+  )
+}
+
+export default App
